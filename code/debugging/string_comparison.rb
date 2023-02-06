1df5616b9a2c87fd577ed74fa0dbd19d93886572
Phil McMinn <phil@owl.local>
2023-02-06 13:18:37 +0000

    Fix debugging examples

--- code/debugging/string_comparison.rb
+++ code/debugging/string_comparison.rb
@@ -1,5 +1,5 @@
 def string_comparison(str1, str2)
-  (0..[str1.length, str2.length].min).each do |i|
+  (0..[str1.length, str2.length].max-1).each do |i|
     return i if str1[i] != str2[i]
   end
   -1
--- code/debugging/string_comparison_buggy_instrumented.rb
+++ code/debugging/string_comparison_buggy_instrumented.rb
@@ -1,5 +1,5 @@
 def string_comparison(str1, str2)
-  (1..[str1.length, str2.length].min).each do |i|
+  (1..[str1.length, str2.length].max-1).each do |i|
     puts "Comparing #{str1[i]} with #{str2[i]}"
     return i if str1[i] != str2[i]
   end
